7888fa95b3d8708ccd856646bbe50d02a5820c2c
M. Talha Jabbar <muhammadtalha61940@gmail.com>
2022-05-25 01:31:00 +0500

    ebs url setup

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -29,8 +29,8 @@ export function Home(props) {
 function FeaturedCars() {
   const [carsList, SetCars] = useState([]);
   if (carsList == "") {
-    //console.log(process.env.REACT_APP_API_URL)
-    fetch(process.env.REACT_APP_API_URL+'/api/auction/auctions_list',{
+    //console.log(process.env.REACT_APP_EBS_URL)
+    fetch(process.env.REACT_APP_EBS_URL+'/api/auction/auctions_list',{
       method:"GET",
       credentials:"include",
     }).then((resonse)=>resonse.json()).then((data)=>{
@@ -44,7 +44,7 @@ function FeaturedCars() {
     return (
       <Col sm="9" style={{ "boxShadow": "5px 10px 5px grey" }} key={auction.regNO}>
         <Card>
-          <CardImg src={process.env.REACT_APP_API_URL+auction.Image} height={"200px"} />
+          <CardImg src={process.env.REACT_APP_EBS_URL+auction.Image} height={"200px"} />
           <CardBody>
             <CardTitle>
               <h4 className="Headings">{auction.name}</h4>
@@ -108,7 +108,7 @@ function Footer() {
   const subscribe = (e, typeID) => {
     e.preventDefault();
 
-    fetch(process.env.REACT_APP_API_URL+'/api/subscription/subscribe', {
+    fetch(process.env.REACT_APP_EBS_URL+'/api/subscription/subscribe', {
       method: 'POST',
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ typeID, UserCNIC: JSON.parse(localStorage.getItem("user")).cnic })
